9495aea123e4a897fc31be1691fb15da89ca527c
bhanu chandra <bhanuchandra2010@live.com>
2018-12-04 17:52:10 +0530

    Initial Baseline

---
+++ ExpenseReportgenerator/ExpenseReportgenerator/BussinessLogic/TransactionDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseReportgenerator.BussinessLogic
+{
+    class TransactionDetail
+    {
+        public TType Transactiontype { get; set; }
+        public float Amount { get; set; }
+        public string BodyName { get; set; }
+
+
+    }
+
+    enum TType
+    {
+        Debit,
+        Credit,
+    }
+}
